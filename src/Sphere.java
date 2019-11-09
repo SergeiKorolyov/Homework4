b4cb96d2bf6369a2bb0a8e2c24834db3c927d145
SergeiKorolyov <korolyov.srg@gmail.com>
2019-11-09 20:32:12 +0300

    Homework

--- src/Sphere.java
+++ src/Sphere.java
@@ -1,6 +1,6 @@
-public class Ball extends Figures {
+public class Spher extends Figures {
 
-    public Ball(String name, int radius) {
+    public Spher(String name, int radius) {
         super(name, radius);
         setArea(4 * Math.PI * Math.pow(radius, 2));
     }
